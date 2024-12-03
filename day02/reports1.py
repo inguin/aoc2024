b023f10a1501e53eec986a3797ea1a5e30b85c84
Ingo van Lil <inguin@gmx.de>
2024-12-03 07:56:44 +0100

    Day 2: Red-Nosed Reports

---
+++ day02/reports1.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python
+
+import sys
+
+filename = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
+
+def is_safe(levels):
+    diff1 = levels[0] - levels[1]
+    if diff1 == 0:
+        return False
+    for l1, l2 in zip(levels, levels[1:]):
+        diff2 = l1 - l2
+        if diff1 < 0 and (diff2 < -3 or diff2 > -1):
+            return False
+        if diff1 > 0 and (diff2 < 1 or diff2 > 3):
+            return False
+    return True
+
+result = 0
+with open(filename) as f:
+    for line in f:
+        levels = [ int(t) for t in line.split() ]
+        if is_safe(levels):
+            result += 1
+print(result)
